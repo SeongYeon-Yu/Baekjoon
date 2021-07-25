504e3b604481dbec77546a02b2be3c7d68307232
seongyeon <ysy2565@naver.com>
2021-07-25 14:18:15 +0900

    step8

--- step8/2775.py
+++ step8/2775.py
@@ -1,11 +1,11 @@
 T = int(input())
 
 for i in range(T):
-  K = int(input())
-  N = int(input())
-  p = [i for i in range(1, N+1)]
+  K = int(input()) #층 
+  N = int(input()) #호
+  p = [i for i in range(1, N+1)] #아래층사람들 수
 
-  for x in range(K):
+  for x in range(K): 
     for y in range(1, N):
-      p[y] += p[y-1]
+      p[y] += p[y-1] 
   print(p[-1])
---
+++ step8/2839.py
@@ -0,0 +1,11 @@
+N = int(input())
+a = 0
+while N>=0:
+  if N % 5 ==0:
+    a += N//5
+    print(a)
+    break
+  N -=3
+  a += 1
+else:
+  print(-1)
