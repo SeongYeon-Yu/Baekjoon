b51cc453e253b49f33d3ad86870687f08d968ad8
seongyeon <ysy2565@naver.com>
2021-07-27 13:04:45 +0900

    step8

--- step8/1011.py
+++ step8/1011.py
@@ -10,6 +10,6 @@ for i in range(n):
   while m_p < a: #총 이동 거리가 이동해야할 거리보다 짧아야하므로 
     cnt +=1 # 이동횟수 1 증가
     m_p += k # x에서 y까지에서 총 k만큼 이동하므로 처음 이동거리에서 더해줌
-    if cnt % 2 == 0: 
-      k += 1
+    if cnt % 2 == 0: # cnt가 2의 배수이면
+      k += 1 # k는 1증가
   print(cnt)
