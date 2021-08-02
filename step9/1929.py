3b2626389fdf3168b363ca1efb48d33174bef1d0
seongyeon <ysy2565@naver.com>
2021-08-02 21:59:17 +0900

    1929 .py

--- step9/1929.py
+++ step9/1929.py
@@ -1,11 +1,11 @@
-m, n = map(int, input().split())
+m, n = map(int, input().split()) #
 
-exp = set()
-primes = []
+exp = set() #제외대상 수를 만들 집합체
+primes = [] #소수 리스트
 for i in range(2, n+1):
     if i in exp:
-        continue
-    if i >= m:
+        continue #제외대상이라면 continue
+    if i >= m: 
         primes.append(i)
     j = 1
     while i * j <= n:
