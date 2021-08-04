8fdf902c27edc7084b7f1788324369ccc9f978c9
seongyeon <ysy2565@naver.com>
2021-08-04 22:04:40 +0900

    4948.py

--- step9/4948.py
+++ step9/4948.py
@@ -1,7 +1,7 @@
 import math
 
 def isPrime(num):
-    if num == 1: return False
+    if num == 1: return False #1은 소수 x
 
     for i in range(2, int(math.sqrt(num))+1):
         if num % i == 0: return False
@@ -10,12 +10,12 @@ def isPrime(num):
 
 
 while(1):
-    answer = 0
+    cnt = 0 # 소수갯수
     n = int(input())
     if n == 0: break
 
     for i in range(n+1, 2*n+1):
         if isPrime(i):
-            answer += 1
+            cnt += 1
 
-    print(answer)
+    print(cnt)
