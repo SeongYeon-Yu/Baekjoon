64f063fc289364802e2f17e5b11788c0e8f00d90
seongyeon <ysy2565@naver.com>
2021-12-04 21:32:51 +0900

    베르트랑 공준

--- step9/4948.py
+++ step9/4948.py
@@ -1,7 +1,9 @@
+
+    
 import math
 
 def isPrime(num):
-    if num == 1: return False #1은 소수 x
+    if num == 1: return False #1은 소수가 아니므로 false
 
     for i in range(2, int(math.sqrt(num))+1):
         if num % i == 0: return False
@@ -9,13 +11,19 @@ def isPrime(num):
     return True
 
 
+li = list(range(2,246912)) #리스트에 123456을 두배한 값을 미리 넣음
+prime_li = []
+for i in li:
+    if isPrime(i):
+        prime_li.append(i)
+
 while(1):
-    cnt = 0 # 소수갯수
+    answer = 0
     n = int(input())
     if n == 0: break
 
-    for i in range(n+1, 2*n+1):
-        if isPrime(i):
-            cnt += 1
+    for i in prime_li:
+        if n < i <= n*2:
+            answer += 1
 
-    print(cnt)
+    print(answer)
